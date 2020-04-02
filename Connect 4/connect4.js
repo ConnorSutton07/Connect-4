6be0a0cd3d2f0d055d3ea3e7e3c16b93131baec1
ConnorSutton07 <55513603+ConnorSutton07@users.noreply.github.com>
2020-04-01 21:36:49 -0500

    0.1
    Everything works, just need to implement a winning system!

--- Connect 4/connect4.js
+++ Connect 4/connect4.js
@@ -1,11 +1,16 @@
 let canvas = document.querySelector('canvas');
+let turnTracker = document.querySelector('h3');
 
 canvas.height = window.innerHeight * (3/4);
 canvas.width = window.innerHeight * (3/4) * (7/6);
 
 let c = canvas.getContext('2d');
 let length = canvas.height * (1/6);
-let radius = Math.sqrt(canvas.width) * 1.2;
+let radius = Math.sqrt(canvas.width) * 1.3;
+let isClicked = false;
+let playerTurn = 'B';
+canvasX = (window.innerWidth - canvas.width) / 2;
+turnTracker.innerText = "Yellow's turn! ";
 
 let mouse = 
 {
@@ -27,12 +32,24 @@ window.addEventListener('resize', () =>
     canvas.width = window.innerHeight * (3/4) * (7/6);
 });
 
-
 class Column
 {
-    constructor()
+    constructor(i)
     {
         this.pieces = ['O', 'O', 'O', 'O', 'O', 'O'];
+        this.order = i;
+        this.right_limit = i * canvas.width * (1/7) + canvasX;
+    }
+    insert(piece)
+    {
+        for (let i = 5; i >= 0; i--)
+        {
+            if (this.pieces[i] == 'O')
+            {
+                this.pieces[i] = piece;
+                break;
+            }
+        }
     }
 }
 
@@ -40,7 +57,7 @@ let board = [];
 
 for (let i = 0; i < 7; i++)
 {
-    board[i] = new Column();
+    board[i] = new Column(i + 1);
 }
 
 function drawBoard()
@@ -51,8 +68,8 @@ function drawBoard()
         {
             c.beginPath();
             c.arc(i * length + (length / 2), j * length + (length / 2), radius, 0, Math.PI * 2, false);
-            if (board[i].pieces[j] == 'B') c.fillStyle = "#2494FF";
-            else if (board[i].pieces[j] == 'R') c.fillStyle = "#FF524F";
+            if (board[i].pieces[j] == 'B') c.fillStyle = "#D9BE36";
+            else if (board[i].pieces[j] == 'R') c.fillStyle = "#F23838";
             else c.fillStyle = "#fffaf0";
             c.fill();
         }
@@ -63,4 +80,34 @@ function animate()
     requestAnimationFrame(animate);
     drawBoard();
 }
+
+window.addEventListener('mousedown', () => {
+    let clickPos = mouse.x;
+    if ((mouse.x >= canvasX) && (mouse.x <= canvas.width + canvasX))
+    {
+        for (let column of board)
+        {
+            if (column.right_limit >= clickPos)
+            {
+                column.insert(playerTurn);
+                if (playerTurn == "B")
+                {
+                    playerTurn = 'R';
+                    turnTracker.innerText = "Red's turn!";
+                    break;
+                }
+                else
+                {
+                    playerTurn = 'B';
+                    turnTracker.innerText = "Yellow's turn!";
+                    break;
+                }
+            }
+        }
+    }
+});
+
+window.addEventListener('mouseup', () => {isClicked = false;});
+
+
 animate();
